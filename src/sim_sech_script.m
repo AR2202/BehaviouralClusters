6137ff201ab54ce808f2c93dce09135a3b681270
Annika Rings <annika.rings@cncb.ox.ac.uk>
2021-05-18 17:29:54 +0100

    added shoving and chasing to sim sech script

--- src/sim_sech_script.m
+++ src/sim_sech_script.m
@@ -1,8 +1,16 @@
+%Annika Rings 2021
+
+%This is a script which runs the Clustering analysis on a specific dataset
+%of simulans and secheillia data
+%This is not a generic reusable script but can be used as a template 
+
 %paths to scripts
 path_to_Clustering ='/home/goodwintracking/AnnikasClusteringScripts/';
 path_to_fast_tsne = '/home/goodwintracking/FIt-SNE-master/';
-addpath(genpath(path_to_Clustering))
-addpath(genpath(path_to_fast_tsne))
+path_to_tracking = '/home/goodwintracking/AnnikasTrackingScripts/';
+addpath(genpath(path_to_Clustering));
+addpath(genpath(path_to_fast_tsne));
+addpath(genpath(path_to_tracking))
 
 %paths and names to files
 datapath = '/mnt/LocalData/Annika_Deniz_Tracking/';
@@ -18,8 +26,18 @@ filenames = {filename_a2a2b_a2a2b,filename_sim13_sim13,...
     filename_a2a2b_sech13,filename_sech13_a2a2b,filename_nuvea_nuvea,...
     filename_praslin_praslin,filename_praslin_nuvea,filename_nuvea_praslin};
 
-%run data preparation script
 cd (datapath);
+%run the pausing function
+pausing();
+%chasing
+run_fraction('chasing',11,'cutoff',(5*pi/6),'below',true,...
+    'wingextonly',false,'additional',12,...
+    'additional_cutoff',(pi/6),'additional_below',true);
+%shoving
+run_fraction('shoving',11,'cutoff',(5*pi/6),'wingextonly',false,...
+    'additional',12,'additional_cutoff',(pi/6),'additional_below',true);
+
+%run data preparation script
 clusterdatafiles = {};
 for filenumber = 1:length(filenames)
     genotypelist = filenames{filenumber};
@@ -28,10 +46,20 @@ for filenumber = 1:length(filenames)
     clusterdatafiles{end+1} = outputname;
     disp('Now preparing file:')
     disp(genotypelist);
+    %pausing
+    find_videos_pausing(genotypelist,genotype);
+    %chasing
+    find_videos_fraction(genotypelist,'fraction','chasing',genotype);
+    %shoving
+    find_videos_fraction(genotypelist,'fraction','shoving',genotype);
+    %clustering
+
     %jaaba data not included because there are some missing data - can be
     %updated once the tracking has produced all jaaba data
-    find_videos_clustering(genotypelist,genotype,'includeotherfly',false,'includejaabadata',false)
+    find_videos_clustering(genotypelist,genotype,...
+        'includeotherfly',false,'includejaabadata',false);
 end
 
-    cluster_data(clusterdatafiles,'sim_sech.mat','kmin',5, 'kmax',30,'both',false)
+    cluster_data(clusterdatafiles,'sim_sech.mat',...
+        'kmin',5, 'kmax',30,'both',false);
 
