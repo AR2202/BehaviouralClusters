4c838b3b597d15ba8e5b26227a4bf3926ed8921d
Annika Rings <annika.rings@cncb.ox.ac.uk>
2021-06-22 15:44:44 +0100

    fixed clustermembers script

--- src/clustermembers.m
+++ src/clustermembers.m
@@ -8,8 +8,9 @@ function clustermembers(filename,outputname,wavelet,opt)
 %%%wavelet: whether wavelet data should be plotted (bool)
 %%%opt: whether k-optimized kmeans should be used or fixed k value (bool)
 
-%probably this is not yet quite what we want as it will depend on the
+%note that frac depend on the
 %number of flies for each genotype
+%frac_genotype is normalized to the number of flies in that genotype
 load(filename);
 if wavelet
     if opt
@@ -37,21 +38,35 @@ else
 end
 colormap cool;
 genotypes = unique(combrescaled_rem_cop(:,size(combrescaled_rem_cop,2)));
-frac=zeros(K,length(genotypes));
-frac_cluster = zeros(length(genotypes),1);
-for clusternumber = 1:K
-    clusterdata = ...
-        combrescaled_rem_cop(KMEANSplot==clusternumber,size(combrescaled_rem_cop,2));
-    total_in_cluster = size(clusterdata,1);
-    
-    for g = 1:length(genotypes)
+frac = zeros(K,length(genotypes));
+absolute = zeros(K,length(genotypes));
+
+frac_genotype = zeros(K,length(genotypes));
+for g = 1:length(genotypes)
     genotype = genotypes(g);
-    genotype_in_cluster = ...
-        size(clusterdata(clusterdata == genotype),1);
-    frac_cluster(g) = genotype_in_cluster/total_in_cluster;
-    frac(clusternumber,g) = frac_cluster(g);
+    frames_in_genotype = ...
+        length(combrescaled_rem_cop(combrescaled_rem_cop(:,size(combrescaled_rem_cop,2)) == genotype,size(combrescaled_rem_cop,2)));
+    
+    for clusternumber = 1:K
+        clusterdata = ...
+            combrescaled_rem_cop(KMEANSplot==clusternumber,size(combrescaled_rem_cop,2));
+        total_in_cluster = size(clusterdata,1);
+        genotype_in_cluster = ...
+            size(clusterdata(clusterdata == genotype),1);
+      
+        frac(clusternumber,g) = genotype_in_cluster/total_in_cluster;
+        absolute(clusternumber,g) = genotype_in_cluster;
+        frac_genotype(clusternumber,g) = genotype_in_cluster/frames_in_genotype;
+        
+        
     end
-    %plotting
+    
+end
+%plotting
+ for clusternumber = 1:K
+    frac_cluster = frac(clusternumber,:);
+    frac_genotype_cluster = frac_genotype(clusternumber,:);
+    %plotting of fraction each genotype contributes to cluster
     fignew=figure('Name',strcat('cluster',num2str(clusternumber)));
     b=bar(frac_cluster,0.1,'FaceColor','flat');
     
@@ -61,8 +76,22 @@ for clusternumber = 1:K
     ylabel 'fraction'
     
     %save figure
-    figname = strcat('members_cluster',num2str(clusternumber),outputname,'.eps');
+    figname = strcat('fraction_in_cluster',num2str(clusternumber),outputname,'.eps');
     saveas(fignew,figname,'epsc');
-    save(strrep(figname,'.eps','.mat'),'frac');
-end
+    
+    %plotting of fraction of the genotype's total frames in this cluster
+    fignew=figure('Name',strcat('fraction per genotype cluster',num2str(clusternumber)));
+    b=bar(frac_genotype_cluster,0.1,'FaceColor','flat');
+    
+    
+    
+    xlabel 'genotype'
+    ylabel 'fraction'
+    %save figure
+    figname = strcat('fraction_of_genotype_cluster',num2str(clusternumber),outputname,'.eps');
+    saveas(fignew,figname,'epsc');
+ end
+ %save data to .mat file
+save(strcat('clustermembers',outputname,'.mat'),'frac','frac_genotype','absolute');
+
 
